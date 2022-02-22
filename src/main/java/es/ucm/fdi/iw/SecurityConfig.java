418ef9b62ad8adf7038f7047afc400c3c78791c6
victor.morenoperez <victor.morenoperez@telefonica.com>
2022-02-22 13:57:19 +0100

    añadido listaPedidos a root

--- src/main/java/es/ucm/fdi/iw/SecurityConfig.java
+++ src/main/java/es/ucm/fdi/iw/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.ignoringAntMatchers("/api/**")
 				.and()
 	        .authorizeRequests()
-	            .antMatchers("/css/**", "/js/**", "/img/**", "/", "/error", "/restaurante", "/platos").permitAll()
+	            .antMatchers("/css/**", "/js/**", "/img/**", "/", "/error", "/listaPedidos","/restaurante", "/platos").permitAll()
 				.antMatchers("/api/**").permitAll()            // <-- public api access	
 				.antMatchers("/admin/**").hasRole("ADMIN")	   // <-- administration
 	            .antMatchers("/user/**").hasRole("USER")	   // <-- logged-in users
