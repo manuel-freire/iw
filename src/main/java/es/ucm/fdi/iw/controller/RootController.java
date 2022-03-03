2ac800ac7cbd893ba2dc0615c2a98bdd1efaaf44
victor.morenoperez <victor.morenoperez@telefonica.com>
2022-03-03 20:07:27 +0100

    modificacion con primera consulta en el index

--- src/main/java/es/ucm/fdi/iw/controller/RootController.java
+++ src/main/java/es/ucm/fdi/iw/controller/RootController.java
@@ -3,17 +3,25 @@ package es.ucm.fdi.iw.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import es.ucm.fdi.iw.model.Label;
 
 /**
  *  Non-authenticated requests only.
  */
 @Controller
 public class RootController {
+
+    @Autowired
+	private EntityManager entityManager;
 
 	private static final Logger log = LogManager.getLogger(RootController.class);
 
@@ -66,8 +74,11 @@ public class RootController {
         filterOptions.add("Precio Ascendente");
         filterOptions.add("Precio Descendente");
         filterOptions.add("Populares");
+        
+        List<Label> labelOptions = entityManager.createQuery("Select x from Label x").getResultList();
 
         model.addAttribute("filterOptions",filterOptions);
+        model.addAttribute("labelOptions",labelOptions);
         return "index";
     }
 }
