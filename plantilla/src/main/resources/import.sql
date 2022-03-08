75b2ed8fd0adcff8a2970da92d8686a6c25ecc14
Manuel Freire <manuel.freire@gmail.com>
2022-03-08 15:51:58 +0100

    Fixed generator used for IDs not initialized
    Without this, the first attempt to insert a new User will fail with a duplicate-primary-key error

--- plantilla/src/main/resources/import.sql
+++ plantilla/src/main/resources/import.sql
@@ -5,3 +5,6 @@ VALUES (1, TRUE, 'ADMIN,USER', 'a',
 INSERT INTO IWUser (id, enabled, roles, username, password)
 VALUES (2, TRUE, 'USER', 'b',
     '{bcrypt}$2a$10$2BpNTbrsarbHjNsUWgzfNubJqBRf.0Vz9924nRSHBqlbPKerkgX.W');
+
+-- start id numbering from a value that is larger than any assigned above
+ALTER SEQUENCE "PUBLIC"."GEN" RESTART WITH 1024;
