b959085c03003d2966892a35508e80ca78d5a97a
Manuel Freire <manuel.freire@fdi.ucm.es>
2025-03-24 12:23:16 +0100

    Ejemplo con simple-datatables

--- plantilla/src/main/java/es/ucm/fdi/iw/controller/AdminController.java
+++ plantilla/src/main/java/es/ucm/fdi/iw/controller/AdminController.java
@@ -1,5 +1,8 @@
 package es.ucm.fdi.iw.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import es.ucm.fdi.iw.model.Lorem;
+import es.ucm.fdi.iw.model.Message;
+import es.ucm.fdi.iw.model.Transferable;
 import es.ucm.fdi.iw.model.User;
 import jakarta.persistence.EntityManager;
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,63 +27,75 @@ import jakarta.servlet.http.HttpSession;
 import jakarta.transaction.Transactional;
 
 /**
- *  Site administration.
+ * Site administration.
  *
- *  Access to this end-point is authenticated - see SecurityConfig
+ * Access to this end-point is authenticated - see SecurityConfig
  */
 @Controller
 @RequestMapping("admin")
 public class AdminController {
 
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
-    @Autowired
-    private EntityManager entityManager;
+  @Autowired
+  private EntityManager entityManager;
 
-    @ModelAttribute
-    public void populateModel(HttpSession session, Model model) {        
-        for (String name : new String[] {"u", "url", "ws"}) {
-            model.addAttribute(name, session.getAttribute(name));
-        }
+  @ModelAttribute
+  public void populateModel(HttpSession session, Model model) {
+    for (String name : new String[] { "u", "url", "ws" }) {
+      model.addAttribute(name, session.getAttribute(name));
     }
+  }
 
-    private static final Logger log = LogManager.getLogger(AdminController.class);
+  private static final Logger log = LogManager.getLogger(AdminController.class);
 
-	@GetMapping("/")
-    public String index(Model model) {
-        log.info("Admin acaba de entrar");
-        model.addAttribute("users", 
-            entityManager.createQuery("select u from User u").getResultList());
-        return "admin";
+  @GetMapping("/")
+  public String index(Model model) {
+    log.info("Admin acaba de entrar");
+    model.addAttribute("users",
+        entityManager.createQuery("select u from User u").getResultList());
+    return "admin";
+  }
+
+  @PostMapping("/toggle/{id}")
+  @Transactional
+  @ResponseBody
+  public String toggleUser(@PathVariable long id, Model model) {
+    log.info("Admin cambia estado de " + id);
+    User target = entityManager.find(User.class, id);
+    target.setEnabled(!target.isEnabled());
+    return "{\"enabled\":" + target.isEnabled() + "}";
+  }
+
+  /**
+   * Returns JSON with all received messages
+   */
+  @GetMapping(path = "all-messages", produces = "application/json")
+  @Transactional // para no recibir resultados inconsistentes
+  @ResponseBody // para indicar que no devuelve vista, sino un objeto (jsonizado)
+  public List<Message.Transfer> retrieveMessages(HttpSession session) {
+    return entityManager.createQuery("select m from Message m", Message.class)
+        .getResultList().stream().map(Transferable::toTransfer)
+        .collect(Collectors.toList());
+  }
+
+  @PostMapping("/populate")
+  @ResponseBody
+  @Transactional
+  public String populate(Model model) {
+    for (int i = 0; i < 10; i++) {
+      User u = new User();
+      u.setUsername("user" + i);
+      u.setPassword(passwordEncoder
+          .encode(UserController
+              .generateRandomBase64Token(9)));
+      u.setEnabled(true);
+      u.setRoles(User.Role.USER.toString());
+      u.setFirstName(Lorem.nombreAlAzar());
+      u.setLastName(Lorem.apellidoAlAzar());
+      entityManager.persist(u);
     }
-
-	@PostMapping("/toggle/{id}")
-    @Transactional
-    @ResponseBody
-    public String toggleUser(@PathVariable long id, Model model) {
-        log.info("Admin cambia estado de " + id);
-        User target = entityManager.find(User.class, id);
-        target.setEnabled(!target.isEnabled());
-        return "{\"enabled\":" + target.isEnabled() + "}";
-    }
-
-    @PostMapping("/populate")
-    @ResponseBody
-    @Transactional
-    public String populate(Model model) {
-        for (int i=0; i<10; i++) {
-            User u = new User();
-            u.setUsername("user" + i);
-            u.setPassword(passwordEncoder
-                .encode(UserController
-                    .generateRandomBase64Token(9)));
-            u.setEnabled(true);
-            u.setRoles(User.Role.USER.toString());
-            u.setFirstName(Lorem.nombreAlAzar());
-            u.setLastName(Lorem.apellidoAlAzar());
-            entityManager.persist(u);
-        }
-        return "{\"admin\": \"populated\"}";
-    }
+    return "{\"admin\": \"populated\"}";
+  }
 }
