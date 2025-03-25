88290da34810cffd05a5fabd289b58c3596e9e09
Manuel Freire <manuel.freire@fdi.ucm.es>
2025-03-25 09:57:00 +0100

    added support for server-side JSg

--- plantilla/src/main/java/es/ucm/fdi/iw/controller/ApiController.java
+++ plantilla/src/main/java/es/ucm/fdi/iw/controller/ApiController.java
@@ -1,55 +1,114 @@
 package es.ucm.fdi.iw.controller;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Map;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.karatelabs.js.Context;
+import io.karatelabs.js.Interpreter;
+import io.karatelabs.js.Node;
+import io.karatelabs.js.Parser;
+import io.karatelabs.js.Source;
 import jakarta.persistence.EntityManager;
 
-
 /**
- *  API, intended for logged-in users.
+ * API, intended for logged-in users.
  *
- *  Access to this end-point is NOT authenticated
- *  - see SecurityConfig and add per-endpoint authentication as needed
+ * Access to this end-point is NOT authenticated
+ * - see SecurityConfig and add per-endpoint authentication as needed
  */
 
-// RestController = all methods annotated with @ResponseBody by default, JSON in, JSON out
+// RestController = all methods annotated with @ResponseBody by default, JSON
+// in, JSON out
 @RestController
 @RequestMapping("api")
 public class ApiController {
 
-    @Autowired
-    private EntityManager entityManager;
+  @Autowired
+  private EntityManager entityManager;
 
-    private static final Logger log = LogManager.getLogger(ApiController.class);
+  private static final Logger log = LogManager.getLogger(ApiController.class);
 
-    /**
-     * Simple status test - returns whatever the message is
-     * @param message
-     * @return {"code" = "<message>"}
-     */
-	@GetMapping("/status/{message}")
-    public Map<String,String> check(@PathVariable String message) {
-        return Map.of("coder", message);
+  /**
+   * Simple status test - returns whatever the message is
+   * 
+   * @param message
+   * @return {"code" = "<message>"}
+   */
+  @GetMapping("/status/{message}")
+  public Map<String, String> check(@PathVariable String message) {
+    return Map.of("coder", message);
+  }
+
+  /**
+   * Counts current users
+   * 
+   * @param message
+   * @return {"code" = "<message>"}
+   */
+  @GetMapping("/users/count")
+  public Map<String, Long> usersCount() {
+    return Map.of("count",
+        (Long) entityManager.createQuery("SELECT COUNT(u) FROM User u").getSingleResult());
+  }
+
+
+  /**
+   * Loads a file from the classpath. 
+   * This works even if the file is in a JAR.
+   * @param path - path to the file - **relative to target/classes**
+   * @return the file
+   */
+  private File loadFromClasspath(String path) {
+      try {
+          return ResourceUtils.getFile("classpath:"+path);
+      } catch (FileNotFoundException e) {
+          throw new RuntimeException("Could not load file from classpath: "+path, e);
+      }
+  }
+
+  /**
+   * Executes JS code using karate-js
+   * @param text
+   * @param vars
+   * @return
+   */
+  private Object eval(String source, Map<String, Object> vars) {
+    Parser parser = new Parser(new Source(source));
+    Node node = parser.parse();
+    Context context = Context.root();
+    if (vars != null) {
+        vars.forEach((k, v) -> context.declare(k, v));
     }
+    return Interpreter.eval(node, context);
+  }
 
-    /**
-     * Counts current users
-     * @param message
-     * @return {"code" = "<message>"}
-     */
-	@GetMapping("/users/count")
-    public Map<String,Long> usersCount() {
-        return Map.of("count", 
-            (Long)entityManager.createQuery("SELECT COUNT(u) FROM User u").getSingleResult());
-    }
+  /** 
+   * Executes JS code loaded from a file in the server
+   */
+  @GetMapping(value = "/js", produces = MediaType.APPLICATION_JSON_VALUE)
+  public Map<String,String> testJs() throws Exception{
+    String start = Files.readString(
+      loadFromClasspath("static/js/js-eval.js").toPath());
+    String source = start + "\n" + "f(v);";
+
+    Object result = eval(source, Map.of(
+      "v", 10, 
+      "exampleExternalVar", "patata"));
+    return Map.of("result", result.toString());
+  }
 }
--- plantilla/src/main/java/es/ucm/fdi/iw/controller/UserController.java
+++ plantilla/src/main/java/es/ucm/fdi/iw/controller/UserController.java
@@ -46,283 +46,282 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
- *  User management.
+ * User management.
  *
- *  Access to this end-point is authenticated.
+ * Access to this end-point is authenticated.
  */
 @Controller()
 @RequestMapping("user")
 public class UserController {
 
-	private static final Logger log = LogManager.getLogger(UserController.class);
+  private static final Logger log = LogManager.getLogger(UserController.class);
 
-	@Autowired
-	private EntityManager entityManager;
+  @Autowired
+  private EntityManager entityManager;
 
-	@Autowired
-    private LocalData localData;
+  @Autowired
+  private LocalData localData;
 
-	@Autowired
-	private SimpMessagingTemplate messagingTemplate;
+  @Autowired
+  private SimpMessagingTemplate messagingTemplate;
 
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+  @Autowired
+  private PasswordEncoder passwordEncoder;
 
-    @ModelAttribute
-    public void populateModel(HttpSession session, Model model) {        
-        for (String name : new String[] {"u", "url", "ws"}) {
-            model.addAttribute(name, session.getAttribute(name));
-        }
+  @ModelAttribute
+  public void populateModel(HttpSession session, Model model) {
+    for (String name : new String[] { "u", "url", "ws" }) {
+      model.addAttribute(name, session.getAttribute(name));
+    }
+  }
+
+  /**
+   * Exception to use when denying access to unauthorized users.
+   * 
+   * In general, admins are always authorized, but users cannot modify
+   * each other's profiles.
+   */
+  @ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "No eres administrador, y éste no es tu perfil") // 403
+  public static class NoEsTuPerfilException extends RuntimeException {
+  }
+
+  /**
+   * Encodes a password, so that it can be saved for future checking. Notice
+   * that encoding the same password multiple times will yield different
+   * encodings, since encodings contain a randomly-generated salt.
+   * 
+   * @param rawPassword to encode
+   * @return the encoded password (typically a 60-character string)
+   *         for example, a possible encoding of "test" is
+   *         {bcrypt}$2y$12$XCKz0zjXAP6hsFyVc8MucOzx6ER6IsC1qo5zQbclxhddR1t6SfrHm
+   */
+  public String encodePassword(String rawPassword) {
+    return passwordEncoder.encode(rawPassword);
+  }
+
+  /**
+   * Generates random tokens. From https://stackoverflow.com/a/44227131/15472
+   * 
+   * @param byteLength
+   * @return
+   */
+  public static String generateRandomBase64Token(int byteLength) {
+    SecureRandom secureRandom = new SecureRandom();
+    byte[] token = new byte[byteLength];
+    secureRandom.nextBytes(token);
+    return Base64.getUrlEncoder().withoutPadding().encodeToString(token); // base64 encoding
+  }
+
+  /**
+   * Landing page for a user profile
+   */
+  @GetMapping("{id}")
+  public String index(@PathVariable long id, Model model, HttpSession session) {
+    User target = entityManager.find(User.class, id);
+    model.addAttribute("user", target);
+    return "user";
+  }
+
+  /**
+   * Alter or create a user
+   */
+  @PostMapping("/{id}")
+  @Transactional
+  public String postUser(
+      HttpServletResponse response,
+      @PathVariable long id,
+      @ModelAttribute User edited,
+      @RequestParam(required = false) String pass2,
+      Model model, HttpSession session) throws IOException {
+
+    User requester = (User) session.getAttribute("u");
+    User target = null;
+    if (id == -1 && requester.hasRole(Role.ADMIN)) {
+      // create new user with random password
+      target = new User();
+      target.setPassword(encodePassword(generateRandomBase64Token(12)));
+      target.setEnabled(true);
+      entityManager.persist(target);
+      entityManager.flush(); // forces DB to add user & assign valid id
+      id = target.getId(); // retrieve assigned id from DB
     }
 
-	/**
-     * Exception to use when denying access to unauthorized users.
-     * 
-     * In general, admins are always authorized, but users cannot modify
-     * each other's profiles.
-     */
-	@ResponseStatus(
-		value=HttpStatus.FORBIDDEN, 
-		reason="No eres administrador, y éste no es tu perfil")  // 403
-	public static class NoEsTuPerfilException extends RuntimeException {}
+    // retrieve requested user
+    target = entityManager.find(User.class, id);
+    model.addAttribute("user", target);
 
-	/**
-	 * Encodes a password, so that it can be saved for future checking. Notice
-	 * that encoding the same password multiple times will yield different
-	 * encodings, since encodings contain a randomly-generated salt.
-	 * @param rawPassword to encode
-	 * @return the encoded password (typically a 60-character string)
-	 * for example, a possible encoding of "test" is 
-	 * {bcrypt}$2y$12$XCKz0zjXAP6hsFyVc8MucOzx6ER6IsC1qo5zQbclxhddR1t6SfrHm
-	 */
-	public String encodePassword(String rawPassword) {
-		return passwordEncoder.encode(rawPassword);
-	}
-
-    /**
-     * Generates random tokens. From https://stackoverflow.com/a/44227131/15472
-     * @param byteLength
-     * @return
-     */
-    public static String generateRandomBase64Token(int byteLength) {
-        SecureRandom secureRandom = new SecureRandom();
-        byte[] token = new byte[byteLength];
-        secureRandom.nextBytes(token);
-        return Base64.getUrlEncoder().withoutPadding().encodeToString(token); //base64 encoding
+    if (requester.getId() != target.getId() &&
+        !requester.hasRole(Role.ADMIN)) {
+      throw new NoEsTuPerfilException();
     }
 
-    /**
-     * Landing page for a user profile
-     */
-	@GetMapping("{id}")
-    public String index(@PathVariable long id, Model model, HttpSession session) {
-        User target = entityManager.find(User.class, id);
+    if (edited.getPassword() != null) {
+      if (!edited.getPassword().equals(pass2)) {
+        log.warn("Passwords do not match - returning to user form");
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
         model.addAttribute("user", target);
         return "user";
+      } else {
+        // save encoded version of password
+        target.setPassword(encodePassword(edited.getPassword()));
+      }
+    }
+    target.setUsername(edited.getUsername());
+    target.setFirstName(edited.getFirstName());
+    target.setLastName(edited.getLastName());
+
+    // update user session so that changes are persisted in the session, too
+    if (requester.getId() == target.getId()) {
+      session.setAttribute("u", target);
     }
 
-    /**
-     * Alter or create a user
-     */
-	@PostMapping("/{id}")
-	@Transactional
-	public String postUser(
-			HttpServletResponse response,
-			@PathVariable long id, 
-			@ModelAttribute User edited, 
-			@RequestParam(required=false) String pass2,
-			Model model, HttpSession session) throws IOException {
+    return "user";
+  }
 
-        User requester = (User)session.getAttribute("u");
-        User target = null;
-        if (id == -1 && requester.hasRole(Role.ADMIN)) {
-            // create new user with random password
-            target = new User();
-            target.setPassword(encodePassword(generateRandomBase64Token(12)));
-            target.setEnabled(true);
-            entityManager.persist(target);
-            entityManager.flush(); // forces DB to add user & assign valid id
-            id = target.getId();   // retrieve assigned id from DB
-        }
-        
-        // retrieve requested user
-        target = entityManager.find(User.class, id);
-        model.addAttribute("user", target);
-		
-		if (requester.getId() != target.getId() &&
-				! requester.hasRole(Role.ADMIN)) {
-			throw new NoEsTuPerfilException();
-		}
-		
-		if (edited.getPassword() != null) {
-            if ( ! edited.getPassword().equals(pass2)) {
-                log.warn("Passwords do not match - returning to user form");
-				response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-				model.addAttribute("user", target);
-        		return "user";
-            } else {
-                // save encoded version of password
-                target.setPassword(encodePassword(edited.getPassword()));
-            }
-		}		
-		target.setUsername(edited.getUsername());
-		target.setFirstName(edited.getFirstName());
-		target.setLastName(edited.getLastName());
+  /**
+   * Returns the default profile pic
+   * 
+   * @return
+   */
+  private static InputStream defaultPic() {
+    return new BufferedInputStream(Objects.requireNonNull(
+        UserController.class.getClassLoader().getResourceAsStream(
+            "static/img/default-pic.jpg")));
+  }
 
-		// update user session so that changes are persisted in the session, too
-        if (requester.getId() == target.getId()) {
-            session.setAttribute("u", target);
-        }
+  /**
+   * Downloads a profile pic for a user id
+   * 
+   * @param id
+   * @return
+   * @throws IOException
+   */
+  @GetMapping("{id}/pic")
+  public StreamingResponseBody getPic(@PathVariable long id) throws IOException {
+    File f = localData.getFile("user", "" + id + ".jpg");
+    InputStream in = new BufferedInputStream(f.exists() ? new FileInputStream(f) : UserController.defaultPic());
+    return os -> FileCopyUtils.copy(in, os);
+  }
 
-		return "user";
-	}	
+  /**
+   * Uploads a profile pic for a user id
+   * 
+   * @param id
+   * @return
+   * @throws IOException
+   */
+  @PostMapping("{id}/pic")
+  @ResponseBody
+  public String setPic(@RequestParam("photo") MultipartFile photo, @PathVariable long id,
+      HttpServletResponse response, HttpSession session, Model model) throws IOException {
 
-    /**
-     * Returns the default profile pic
-     * 
-     * @return
-     */
-    private static InputStream defaultPic() {
-	    return new BufferedInputStream(Objects.requireNonNull(
-            UserController.class.getClassLoader().getResourceAsStream(
-                "static/img/default-pic.jpg")));
+    User target = entityManager.find(User.class, id);
+    model.addAttribute("user", target);
+
+    // check permissions
+    User requester = (User) session.getAttribute("u");
+    if (requester.getId() != target.getId() &&
+        !requester.hasRole(Role.ADMIN)) {
+      throw new NoEsTuPerfilException();
     }
 
-    /**
-     * Downloads a profile pic for a user id
-     * 
-     * @param id
-     * @return
-     * @throws IOException
-     */
-    @GetMapping("{id}/pic")
-    public StreamingResponseBody getPic(@PathVariable long id) throws IOException {
-        File f = localData.getFile("user", ""+id+".jpg");
-        InputStream in = new BufferedInputStream(f.exists() ?
-            new FileInputStream(f) : UserController.defaultPic());
-        return os -> FileCopyUtils.copy(in, os);
+    log.info("Updating photo for user {}", id);
+    File f = localData.getFile("user", "" + id + ".jpg");
+    if (photo.isEmpty()) {
+      log.info("failed to upload photo: emtpy file?");
+    } else {
+      try (BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(f))) {
+        byte[] bytes = photo.getBytes();
+        stream.write(bytes);
+        log.info("Uploaded photo for {} into {}!", id, f.getAbsolutePath());
+      } catch (Exception e) {
+        response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+        log.warn("Error uploading " + id + " ", e);
+      }
     }
+    return "{\"status\":\"photo uploaded correctly\"}";
+  }
 
-    /**
-     * Uploads a profile pic for a user id
-     * 
-     * @param id
-     * @return
-     * @throws IOException
+  @GetMapping("error")
+  public String error(Model model, HttpSession session, HttpServletRequest request) {
+    model.addAttribute("sess", session);
+    model.addAttribute("req", request);
+    return "error";
+  }
+
+  /**
+   * Returns JSON with all received messages
+   */
+  @GetMapping(path = "received", produces = "application/json")
+  @Transactional // para no recibir resultados inconsistentes
+  @ResponseBody // para indicar que no devuelve vista, sino un objeto (jsonizado)
+  public List<Message.Transfer> retrieveMessages(HttpSession session) {
+    long userId = ((User) session.getAttribute("u")).getId();
+    User u = entityManager.find(User.class, userId);
+    log.info("Generating message list for user {} ({} messages)",
+        u.getUsername(), u.getReceived().size());
+    return u.getReceived().stream().map(Transferable::toTransfer).collect(Collectors.toList());
+  }
+
+  /**
+   * Returns JSON with count of unread messages
+   */
+  @GetMapping(path = "unread", produces = "application/json")
+  @ResponseBody
+  public String checkUnread(HttpSession session) {
+    long userId = ((User) session.getAttribute("u")).getId();
+    long unread = entityManager.createNamedQuery("Message.countUnread", Long.class)
+        .setParameter("userId", userId)
+        .getSingleResult();
+    session.setAttribute("unread", unread);
+    return "{\"unread\": " + unread + "}";
+  }
+
+  /**
+   * Posts a message to a user.
+   * 
+   * @param id of target user (source user is from ID)
+   * @param o  JSON-ized message, similar to {"message": "text goes here"}
+   * @throws JsonProcessingException
+   */
+  @PostMapping("/{id}/msg")
+  @ResponseBody
+  @Transactional
+  public String postMsg(@PathVariable long id,
+      @RequestBody JsonNode o, Model model, HttpSession session)
+      throws JsonProcessingException {
+
+    String text = o.get("message").asText();
+    User u = entityManager.find(User.class, id);
+    User sender = entityManager.find(
+        User.class, ((User) session.getAttribute("u")).getId());
+    model.addAttribute("user", u);
+
+    // construye mensaje, lo guarda en BD
+    Message m = new Message();
+    m.setRecipient(u);
+    m.setSender(sender);
+    m.setDateSent(LocalDateTime.now());
+    m.setText(text);
+    entityManager.persist(m);
+    entityManager.flush(); // to get Id before commit
+
+    ObjectMapper mapper = new ObjectMapper();
+    /*
+     * // construye json: método manual
+     * ObjectNode rootNode = mapper.createObjectNode();
+     * rootNode.put("from", sender.getUsername());
+     * rootNode.put("to", u.getUsername());
+     * rootNode.put("text", text);
+     * rootNode.put("id", m.getId());
+     * String json = mapper.writeValueAsString(rootNode);
      */
-    @PostMapping("{id}/pic")
-	@ResponseBody
-    public String setPic(@RequestParam("photo") MultipartFile photo, @PathVariable long id, 
-        HttpServletResponse response, HttpSession session, Model model) throws IOException {
+    // persiste objeto a json usando Jackson
+    String json = mapper.writeValueAsString(m.toTransfer());
 
-        User target = entityManager.find(User.class, id);
-        model.addAttribute("user", target);
-		
-		// check permissions
-		User requester = (User)session.getAttribute("u");
-		if (requester.getId() != target.getId() &&
-				! requester.hasRole(Role.ADMIN)) {
-            throw new NoEsTuPerfilException();
-		}
-		
-		log.info("Updating photo for user {}", id);
-		File f = localData.getFile("user", ""+id+".jpg");
-		if (photo.isEmpty()) {
-			log.info("failed to upload photo: emtpy file?");
-		} else {
-			try (BufferedOutputStream stream =
-					new BufferedOutputStream(new FileOutputStream(f))) {
-				byte[] bytes = photo.getBytes();
-				stream.write(bytes);
-                log.info("Uploaded photo for {} into {}!", id, f.getAbsolutePath());
-			} catch (Exception e) {
-                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-				log.warn("Error uploading " + id + " ", e);
-			}
-		}
-		return "{\"status\":\"photo uploaded correctly\"}";
-    }
+    log.info("Sending a message to {} with contents '{}'", id, json);
 
-	@GetMapping("error")
-	public String error(Model model, HttpSession session, HttpServletRequest request) {
-		model.addAttribute("sess", session);
-		model.addAttribute("req", request);
-		return "error";
-	}
-
-
-    /**
-     * Returns JSON with all received messages
-     */
-    @GetMapping(path = "received", produces = "application/json")
-	@Transactional // para no recibir resultados inconsistentes
-	@ResponseBody  // para indicar que no devuelve vista, sino un objeto (jsonizado)
-	public List<Message.Transfer> retrieveMessages(HttpSession session) {
-		long userId = ((User)session.getAttribute("u")).getId();		
-		User u = entityManager.find(User.class, userId);
-		log.info("Generating message list for user {} ({} messages)", 
-				u.getUsername(), u.getReceived().size());
-		return  u.getReceived().stream().map(Transferable::toTransfer).collect(Collectors.toList());
-	}	
-    
-    /**
-     * Returns JSON with count of unread messages 
-     */
-	@GetMapping(path = "unread", produces = "application/json")
-	@ResponseBody
-	public String checkUnread(HttpSession session) {
-		long userId = ((User)session.getAttribute("u")).getId();		
-		long unread = entityManager.createNamedQuery("Message.countUnread", Long.class)
-			.setParameter("userId", userId)
-			.getSingleResult();
-		session.setAttribute("unread", unread);
-		return "{\"unread\": " + unread + "}";
-    }
-    
-    /**
-     * Posts a message to a user.
-     * @param id of target user (source user is from ID)
-     * @param o JSON-ized message, similar to {"message": "text goes here"}
-     * @throws JsonProcessingException
-     */
-    @PostMapping("/{id}/msg")
-	@ResponseBody
-	@Transactional
-	public String postMsg(@PathVariable long id, 
-			@RequestBody JsonNode o, Model model, HttpSession session) 
-		throws JsonProcessingException {
-		
-		String text = o.get("message").asText();
-		User u = entityManager.find(User.class, id);
-		User sender = entityManager.find(
-				User.class, ((User)session.getAttribute("u")).getId());
-		model.addAttribute("user", u);
-		
-		// construye mensaje, lo guarda en BD
-		Message m = new Message();
-		m.setRecipient(u);
-		m.setSender(sender);
-		m.setDateSent(LocalDateTime.now());
-		m.setText(text);
-		entityManager.persist(m);
-		entityManager.flush(); // to get Id before commit
-		
-		ObjectMapper mapper = new ObjectMapper();
-		/*
-		// construye json: método manual
-		ObjectNode rootNode = mapper.createObjectNode();
-		rootNode.put("from", sender.getUsername());
-		rootNode.put("to", u.getUsername());
-		rootNode.put("text", text);
-		rootNode.put("id", m.getId());
-		String json = mapper.writeValueAsString(rootNode);
-		*/
-		// persiste objeto a json usando Jackson
-		String json = mapper.writeValueAsString(m.toTransfer());
-
-		log.info("Sending a message to {} with contents '{}'", id, json);
-
-		messagingTemplate.convertAndSend("/user/"+u.getUsername()+"/queue/updates", json);
-		return "{\"result\": \"message sent.\"}";
-	}	
+    messagingTemplate.convertAndSend("/user/" + u.getUsername() + "/queue/updates", json);
+    return "{\"result\": \"message sent.\"}";
+  }
 }
