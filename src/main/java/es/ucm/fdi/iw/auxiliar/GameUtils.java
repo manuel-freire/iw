00ae97e27e288b7372be7678f3e66bbaec4b8b67
VictorSS7 <victor.ssandu17@gmail.com>
2026-02-25 16:20:10 +0100

    Sin Ñ

--- src/main/java/es/ucm/fdi/iw/auxiliar/GameUtils.java
+++ src/main/java/es/ucm/fdi/iw/auxiliar/GameUtils.java
@@ -4,7 +4,7 @@ import java.security.SecureRandom;
 import java.util.stream.Collectors;
 
 public class GameUtils {
-    private static final String alfabeto = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ1234567890";
+    private static final String alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
     private static final SecureRandom random = new SecureRandom();
 
     public static String generateRandomCode(int length){
