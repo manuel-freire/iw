813f6b277011af07f223871e710d44a148e3c8d6
VictorSS7 <victor.ssandu17@gmail.com>
2026-03-24 22:14:27 +0100

    Avances del leaderboard

--- src/main/java/es/ucm/fdi/iw/repository/ScoreRepository.java
+++ src/main/java/es/ucm/fdi/iw/repository/ScoreRepository.java
@@ -4,9 +4,11 @@ import es.ucm.fdi.iw.model.Score;
 import es.ucm.fdi.iw.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ScoreRepository extends JpaRepository<Score, Long> {
   Optional<Score> findByUser(User user);
+  List<Score> findAllByOrderByTotalPointsDesc();
 }
 
